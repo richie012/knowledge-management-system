a80d29ddc93ac34459780fcbd66d87c9d968cbed
richie <82734398+richie012@users.noreply.github.com>
2023-11-06 19:25:05 +0500

    Add files via upload

---
+++ src/main/java/ru/richieernest/knowledgeManagementSystem/KnowledgeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.richieernest.knowledgeManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/richieernest/knowledgeManagementSystem/KnowledgeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.richieernest.knowledgeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
