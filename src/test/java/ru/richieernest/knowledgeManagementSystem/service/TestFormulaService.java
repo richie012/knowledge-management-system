8ba490eed018abaef0d189426962a352ee06b396
richie012 <kykevich.maks@gmail.com>
2024-04-08 22:07:44 +0500

    тест

--- src/test/java/ru/richieernest/knowledgeManagementSystem/service/TestFormulaService.java
+++ src/test/java/ru/richieernest/knowledgeManagementSystem/service/TestFormulaService.java
@@ -2,14 +2,12 @@ package ru.richieernest.knowledgeManagementSystem.service;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import ru.richieernest.knowledgeManagementSystem.entity.Formula;
 import ru.richieernest.knowledgeManagementSystem.mapper.FormulaMapper;
 import ru.richieernest.knowledgeManagementSystem.repository.FormulaDependenciesMapRepo;
 import ru.richieernest.knowledgeManagementSystem.repository.FormulaRepo;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
